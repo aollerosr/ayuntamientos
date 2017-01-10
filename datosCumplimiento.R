7f05b2242acc691be30879c21d2909dc23bed6dc
aollerosr <aollerosr@yahoo.es>
2017-01-10 23:33:46 +0100

    Add files via upload

---
+++ datosCumplimiento.R
@@ -0,0 +1,88 @@
+#############################################################################
+#### Datos de id-NIF ayuntamientos + cumplimiento
+#### Los datos se obtienen a partir de pythong
+
+a1 <- read.csv("/home/antonio/Python/resultados2013", header = FALSE)
+a2 <- read.csv("/home/antonio/Python/resultadosaaa2013", header = FALSE)
+
+a1 <- a1[!is.na(a1$V2),]
+a2 <- a2[!is.na(a2$V2),]
+
+df_cumplimiento2013 <- rbind(a1,a2)
+
+remove(a1)
+remove(a2)
+
+colnames(df_cumplimiento2013) <- c("rendicionCuentasID", "NIF", "tipoYNombre")
+
+df_cumplimiento2014 <- read.csv("/home/antonio/ayuntamientos/datos/NIFs_2014.csv")
+colnames(df_cumplimiento2014) <- c("rendicionCuentasID", "NIF")
+df_cumplimiento2014$tipoYNombre <- ""
+
+df_cumplimiento2015 <- read.csv("/home/antonio/ayuntamientos/datos/NIFs_2015.csv")
+colnames(df_cumplimiento2015) <- c("rendicionCuentasID", "NIF")
+df_cumplimiento2015$tipoYNombre <- ""
+
+df_cumplimiento <- rbind(df_cumplimiento2013, df_cumplimiento2014, df_cumplimiento2015)
+
+df_cumplimiento <- unique(df_cumplimiento[,c("NIF", "rendicionCuentasID")])
+
+
+df_cumplimiento2013$cumple2013 <- "TRUE"
+df_cumplimiento2014$cumple2014 <- "TRUE"
+df_cumplimiento2015$cumple2015 <- "TRUE"
+
+df_cumplimiento <- merge.data.frame(df_cumplimiento,
+                                    df_cumplimiento2013[, c("NIF", "tipoYNombre" ,"cumple2013")],
+                                    by = "NIF",
+                                    all.x = TRUE)
+
+
+
+df_cumplimiento <- merge.data.frame(df_cumplimiento,
+                                    df_cumplimiento2014[, c("NIF", "cumple2014")],
+                                    by = "NIF",
+                                    all.x = TRUE)
+
+df_cumplimiento <- merge.data.frame(df_cumplimiento,
+                                    df_cumplimiento2015[, c("NIF", "cumple2015")],
+                                    by = "NIF",
+                                    all.x = TRUE)
+
+
+
+setdiff(df_cumplimiento2014$NIF, df_cumplimiento$NIF)
+setdiff(df_cumplimiento2015$NIF, df_cumplimiento$NIF)
+
+
+remove(df_cumplimiento2013)
+remove(df_cumplimiento2014)
+remove(df_cumplimiento2015)
+
+###################################################
+#Parser para separar nombre y tipo de entidad
+#Por terminar
+
+parseTipo <- function(strng){
+  switch(substr(strng,1,3),
+         "Ayu" = substr(strng,1,10),
+         "Dip" = substr(strng,1,10),
+         "Con" = substr(strng,1,10),
+         "Man" = substr(strng,1,10),
+         "Cab" = substr(strng,1,10),
+         "Com" = substr(strng,1,10), 
+         "Agr" = substr(strng,1,10),    
+         "Áre" = substr(strng,1,10)    
+  )
+}
+
+df_cumplimiento$Tipo <- 1
+for(i in 1: length(df_cumplimiento$tipoYNombre)){
+  df_cumplimiento[i,"Tipo"] <- parseTipo(df_cumplimiento[i,"tipoYNombre"])
+}
+
+
+
+
+
+write.csv(df_cumplimiento, file = paste0(path, "/", "cumplimiento.csv"), fileEncoding = "UTF-8")
